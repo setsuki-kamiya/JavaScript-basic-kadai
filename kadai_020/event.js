e2db96f8ed650e56354ca65d2ea94b526d941a2e
setsuki-kamiya <165028453+setsuki-kamiya@users.noreply.github.com>
2024-07-02 21:14:19 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,11 +1,12 @@
 // HTML要素をidで取得し、中身を出力する
 const text =document.getElementById('text');
+const btn =document.getElementById('btn');
 btn.addEventListener('click',()=>{
   text.textContent ='ボタンをクリックしました'
 });
 
 console.log(text);
-
+console.log('btn');
 
 
 
